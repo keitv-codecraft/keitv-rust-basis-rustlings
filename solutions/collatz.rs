a6e212a168a73c54d64e8d371a31e525998d3cf9
Ralph Langendam <rlangendam@gmail.com>
2026-07-10 00:03:00 +0200

    idiomatische versie (mogelijk reverten)

--- solutions/collatz.rs
+++ solutions/collatz.rs
@@ -28,23 +28,6 @@ fn next_collatz_safe(n: u128) -> Option<u128> {
     }
 }
 
-struct CollatzIterator(Option<u128>);
-
-impl Iterator for CollatzIterator {
-    type Item = u128;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        self.0 = self.0.and_then(|value| next_collatz_safe(value));
-        self.0
-    }
-}
-
-impl CollatzIterator {
-    fn new(value: u128) -> Self {
-        Self(Some(value))
-    }
-}
-
 fn main() {
     // We gaan achtereenvolgens alle positieve gehele getallen af
     // en we slaan voor ieder getal het aantal stappen op
@@ -53,49 +36,49 @@ fn main() {
     // We houden alle getallen bij waar we langs komen, zodat we
     // in een keer het aantal stappen voor al deze getallen kunnen
     // bepalen.
-    let mut sequence = Vec::new();
-    for mut n in 1..1_000_000 {
+    for n in 1..10 {
         // Als we het getal al in een eerdere reeks langs hebben zien
         // komen hoeven we niets te berekenen, want het aantal stappen
         // is al bekend.
         if cache.get(&n).is_some() {
             continue;
         }
-        let mut step_count = Some(0);
-        while n != 1 {
-            sequence.push(n);
-            if let Some(maybe_count) = cache.get(&n) {
-                step_count = match (step_count, maybe_count) {
-                    // Als we gaandeweg een getal tegen komen dat al
-                    // in de cache zit, dan weten we al hoeveel extra
-                    // stappen we nog moeten zetten om op 1 uit te komen
-                    (Some(inner), Some(count)) => Some(inner + count),
-                    // Als dit aantal stappen onbekend is, dan is het
-                    // aantal stappen voor alle getallen in de reeks onbekend
-                    _ => None,
-                };
-                break;
-            }
-            if let Some(next) = next_collatz_safe(n) {
-                n = next;
-                step_count = step_count.map(|inner| inner + 1);
+
+        let mut iter = std::iter::successors(Some(n), |m| next_collatz_safe(*m)).peekable();
+        let sequence: Vec<_> = std::iter::from_fn(|| {
+            if iter
+                .peek()
+                .map_or(false, |m| cache.get(m).is_none() && n != 1)
+            {
+                iter.next()
             } else {
-                println!("Overflow: {n}");
-                step_count = None;
-                break;
+                iter.next_if(|_| true)
             }
-        }
-        if let Some(step_count) = step_count {
-            for (i, n) in sequence.iter().enumerate() {
-                let step_count = step_count - i;
-                cache.insert(*n, Some(step_count));
+        })
+        .collect();
+
+        let last_steps = if let Some(last) = sequence.last() {
+            if *last == 1 {
+                Some(0)
+            } else if let Some(maybe_count) = cache.get(last) {
+                *maybe_count
+            } else {
+                dbg!(last);
+                None
             }
         } else {
-            for n in &sequence {
-                cache.insert(*n, None);
+            None
+        };
+
+        if let Some(last_steps) = last_steps {
+            for (m, count) in sequence.into_iter().rev().skip(1).zip(last_steps..) {
+                cache.insert(m, Some(count));
+            }
+        } else {
+            for m in sequence {
+                cache.insert(m, None);
             }
         }
-        sequence.clear();
     }
     let mut c: Vec<_> = cache
         .iter()
